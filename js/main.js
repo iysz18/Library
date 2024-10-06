2c114b360ae22fa6e9377afd0f3e004f5b6539e7
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-10-06 21:42:22 +0200

    Needed logic for working button to add book implemented.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // Wait for DOM to load
 document.addEventListener("DOMContentLoaded", () => {
     const dialogWindow = document.querySelector("dialog");
+    const addBookBtn = document.querySelector(".add-book");
 
-    dialogWindow.showModal();
+    addBookBtn.addEventListener("click", () => {
+        dialogWindow.showModal();
+    });
 });
