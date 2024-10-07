1f19d1ba5c9073fa8afb8dfb32d158f99aedac37
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-10-07 12:43:38 +0200

    Adding/Creating book works, myLibrary array getting populated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-// Wait for DOM to load
 document.addEventListener("DOMContentLoaded", () => {
-    const dialogWindow = document.querySelector("dialog");
-    const addBookBtn = document.querySelector(".add-book");
+    // open the book register dialog
+    const registerBookBtn = document.querySelector(".openBookDialog");
+    const registerBookDialog = document.querySelector("dialog");
 
-    addBookBtn.addEventListener("click", () => {
-        dialogWindow.showModal();
-    });
+    const myLibrary = [];
+
+    function Book(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = readStatus;
+    }
+
+    function addBookToLibrary(title, author, pages, readStatus) {
+        let newBook = new Book(title, author, pages, readStatus);
+        myLibrary.push(newBook);
+        for (let book in myLibrary) console.log(myLibrary[book]);
+    }
+    
+
+    registerBookBtn.addEventListener("click", () => registerBookDialog.showModal());
+
+        /* By clicking on the add book button inside the dialog,
+            we add the title, author, pages and read status info */ 
+        const addToLibrary = document.querySelector(".addToLibrary");
+        addToLibrary.addEventListener("click", () => {
+            const titleInfo = document.querySelector("#title");
+            const authorInfo = document.querySelector("#author");
+            const pagesInfo = document.querySelector("#pages");
+            const readInfo = document.querySelector("#read-status");
+
+            addBookToLibrary(titleInfo.value, authorInfo.value, pagesInfo.value, readInfo.checked);
+        });
+    
 });
