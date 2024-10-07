b8a911613a4f9814fff39a033061a2fca6585275
Yasin Aydas <115567753+iysz18@users.noreply.github.com.>
2024-10-07 12:53:29 +0200

    Displaying new created books on the bookshelf works now

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const myLibrary = [];
 
+    // Book Constructor function to create book objects
     function Book(title, author, pages, readStatus) {
         this.title = title;
         this.author = author;
@@ -12,13 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
         this.read = readStatus;
     }
 
+    // add the book to the library by creating the book with the Book {}
     function addBookToLibrary(title, author, pages, readStatus) {
         let newBook = new Book(title, author, pages, readStatus);
         myLibrary.push(newBook);
         for (let book in myLibrary) console.log(myLibrary[book]);
     }
     
-
+    // Register Btn to register the new book
     registerBookBtn.addEventListener("click", () => registerBookDialog.showModal());
 
         /* By clicking on the add book button inside the dialog,
@@ -30,7 +32,22 @@ document.addEventListener("DOMContentLoaded", () => {
             const pagesInfo = document.querySelector("#pages");
             const readInfo = document.querySelector("#read-status");
 
+            // Finally, add the book to the library by calling the function
             addBookToLibrary(titleInfo.value, authorInfo.value, pagesInfo.value, readInfo.checked);
+
+            // After book got created/added to library, show it as a card in the bookShelf div
+            const bookShelfContainer = document.querySelector(".bookshelf");
+
+            // Create dynamically new bookInfo cards
+            const newBookCard = document.createElement('div');
+            newBookCard.classList.add("bookCardInfo");
+            const newBookInfo = document.createElement('p');
+            newBookInfo.innerHTML = `Title: ${titleInfo.value}<br>
+                                     Author: ${authorInfo.value}<br>
+                                     Pages: ${pagesInfo.value}<br>
+                                     Read: ${readInfo.value}<br>`;
+            newBookCard.appendChild(newBookInfo);
+            bookShelfContainer.appendChild(newBookCard);
+
         });
-    
 });
